ad49fd9184d59792cff3750a35029ff7e0796c56
qelvyn99 <132350650+qelvyn99@users.noreply.github.com>
2024-07-03 21:36:53 +0000

    update

---
+++ 0x04-TypeScript/task_3/js/main.ts
@@ -0,0 +1,11 @@
+/// <reference path="./crud.d.ts" />
+import { RowID, RowElement } from './interface';
+import * as CRUD from './crud';
+
+const row: RowElement = { firstName: 'Steven', lastName: 'Papers' };
+const newRowID: RowID = CRUD.insertRow(row);
+
+const updatedRow: RowElement = { ...row, age: 23 };
+CRUD.updateRow(newRowID, updatedRow);
+
+CRUD.deleteRow(newRowID);
